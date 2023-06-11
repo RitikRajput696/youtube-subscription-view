a03ab1305e2112b19d41f6192b6c340835c5e905
ritikrajput696 <rixusinput696@gmail.com>
2023-06-11 15:48:30 +0530

    adding colors to videos boxes

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const subscriptionView = document.querySelector('.subcription-view');
+const videos = document.querySelectorAll('.subcription-view .vid');
 const gridBtn = document.getElementById('grid');
 const listBtn = document.getElementById('list');
+
+const bgColors = [
+  "ff3432",
+  "4da6ff",
+  "9966ff",
+  "cc00ff",
+]
+
+console.log(videos);
+
+videos.forEach(video => {
+  const colorItem = bgColors[Math.floor(Math.random() * bgColors.length)];
+  video.style.backgroundColor = "#" + colorItem;
+});
 
 listBtn.addEventListener('click', () => {
   console.log;
